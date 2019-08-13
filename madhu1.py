4fd0a6f73edffdb2befe01554da52f06faf99e95
Madhumathii <52281595+Madhumathii@users.noreply.github.com>
2019-08-12 22:01:08 -0700

    Create madhu1.py

---
+++ madhu1.py
@@ -0,0 +1,10 @@
+a = 5
+fact = 1
+if a < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif a == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,a + 1):
+       fact = fact*i
+   print("120")
